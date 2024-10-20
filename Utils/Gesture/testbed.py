e4a0144eb8f9a2b026bf13d044fdcc3d9f232b04
RenatoGallicola <renato.gallicola2001@gmail.com>
2024-10-20 15:43:15 +0200

    Hand gesture implementation

---
+++ Utils/Gesture/testbed.py
@@ -0,0 +1,9 @@
+from hand_gesture_recognition import HandGesture as hg
+
+num_hands=2
+enable_pointer=True
+show_fps=True
+show_bounding_box=True
+show_info_box=True
+
+hg.start(num_hands,enable_pointer, show_fps, show_bounding_box, show_info_box)
